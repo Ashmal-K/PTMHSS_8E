b4afaed9c963194f3f57e7773e6d6bdf57708df3
Ashmal <karangaden2008@gmail.com>
2022-11-13 11:44:53 +0530

    u

---
+++ index.js
@@ -0,0 +1,40 @@
+window.onscroll = function () { myFunction() };
+var header = document.getElementById('header')
+var sticky = header.offsetTop;
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add('bgc')
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove("sticky");
+        header.classList.remove("bgc");
+    }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000);
+}
